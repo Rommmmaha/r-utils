a787b1b57691b22d9bb4250e40ed973aa63d8e35
Rommmmaha <Rommmmaha@gmail.com>
2026-05-09 10:54:27 +0300

    update r_netstatus

--- r_netstatus/src/main.rs
+++ r_netstatus/src/main.rs
@@ -1,13 +1,23 @@
 use chrono::Local;
 use colored::Colorize;
+use std::env;
 use std::io::{self, Write};
-use std::net::{SocketAddr, TcpStream};
+use std::net::{TcpStream, ToSocketAddrs};
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
 fn main() {
-    let target_addr: SocketAddr = "1.1.1.1:53".parse().unwrap();
-    let timeout = Duration::from_secs(1);
+    let input = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "1.1.1.1:53".to_string());
+    let target_addr = input
+        .to_socket_addrs()
+        .expect(
+            "Failed to parse or resolve address (ensure you include the port, e.g., domain.com:80)",
+        )
+        .next()
+        .expect("No socket addresses resolved");
+    let timeout = Duration::from_secs(2);
     let mut last_status: Option<bool> = None;
     let mut last_change_instant = Instant::now();
     loop {
@@ -21,7 +31,7 @@ fn main() {
                 let hh = secs / 3600;
                 let mm = (secs % 3600) / 60;
                 let ss = secs % 60;
-                let output = format!("({:02}:{:02}:{:02})", hh, mm, ss);
+                let output = format!("{:02}:{:02}:{:02}", hh, mm, ss);
                 if last_status.unwrap() {
                     println!("{}", output.green());
                 } else {
@@ -29,19 +39,19 @@ fn main() {
                 }
             }
             let timestamp = Local::now().format("%H:%M:%S").to_string();
-            let output = format!("[{}]", timestamp);
+            let output = format!("{}", timestamp);
             if is_success {
-                println!("{} +", output.green());
+                print!("{}│Online │", output.green());
             } else {
-                println!("{} -", output.red());
+                print!("{}│Offline│", output.red());
             }
             io::stdout().flush().unwrap();
             last_status = Some(is_success);
             last_change_instant = now_instant;
         }
         let elapsed = loop_start.elapsed();
-        if elapsed < Duration::from_secs(1) {
-            sleep(Duration::from_secs(1) - elapsed);
+        if elapsed < timeout {
+            sleep(timeout - elapsed);
         }
     }
 }
